2f232494df19d4acd3be08e59d610c5ee20e00bc
jrod730 <jrodr100@neiu.edu>
2017-10-03 10:56:36 -0500

    Still need to read file from resources.

---
+++ app/src/test/java/PlayerDisplay.java
@@ -0,0 +1,19 @@
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.jrod730.dfsoptimizer.R;
+
+/**
+ * Created by jrod730 on 9/19/17.
+ */
+
+public class PlayerDisplay extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playerdisplay);
+
+
+    }
+}
---
+++ app/src/test/java/ValueCreation.java
@@ -0,0 +1,6 @@
+/**
+ * Created by jrod730 on 9/19/17.
+ */
+
+public class ValueCreation {
+}
